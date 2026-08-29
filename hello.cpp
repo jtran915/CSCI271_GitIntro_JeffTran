f0e6dc8009c96c73bf8cbf7e871e19bef28e2df4
jtran915 <jefferson918785@my.yosemite.edu>
2026-08-28 19:46:18 -0700

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: JeffersonTran
+// Section: CSCI-271-1290
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
